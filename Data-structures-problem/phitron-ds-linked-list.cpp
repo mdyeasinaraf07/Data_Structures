481f946ccdcf6f71f2622ae55aedf4869025ea82
Md Yeasin Arafat <116306992+mdyeasinaraf07@users.noreply.github.com>
2023-03-02 22:38:54 +0600

    Add files via upload

---
+++ Data-structures-problem/phitron-ds-linked-list.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* next;
+
+    Node(int value){
+        this-> data = value;
+        this-> next = NULL;
+    }
+};
+void insert_at_head(Node*& head, int value){
+    Node* create_new_node = new Node(value);
+    if(head == NULL){
+        head = create_new_node;
+        return;
+    }
+    else{
+        create_new_node-> next = head;
+        head = create_new_node;
+    }
+}
+void print_linked_list(Node* head){
+    while(head != NULL){
+        cout<< head-> data<< endl;
+        head = head-> next;
+    }
+}
+
+int main(){
+    Node* head = NULL;
+    insert_at_head(head, 5);
+    insert_at_head(head, 10);
+    insert_at_head(head, 15);
+    insert_at_head(head, 20);
+
+    print_linked_list(head);
+
+    return 0;
+}
