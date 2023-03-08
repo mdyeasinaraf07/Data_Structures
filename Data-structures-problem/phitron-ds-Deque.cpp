1da2d1c25b8d910c65303c6de19b6f849baf7e50
Md Yeasin Arafat <116306992+mdyeasinaraf07@users.noreply.github.com>
2023-03-08 18:11:14 +0600

    Add files via upload

---
+++ Data-structures-problem/phitron-ds-Deque.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    deque <int> dq;
+
+    dq.push_back(10);
+    dq.push_back(20);
+    dq.push_back(30);
+    dq.push_back(40);
+
+    cout<< dq.front()<< "\n";
+    cout<< dq.size()<< "\n";
+
+    dq.push_front(50);
+    cout<< dq.front()<< "\n";
+
+    dq.pop_back();
+    dq.pop_front();
+    cout<< dq.back()<< "\n";
+
+
+    return 0;
+}
